3dc84176d5c96054760c4b72a1c1c8bbbd126a3e
J153586 <J153586@tafe.wa.edu.au>
2024-11-08 22:30:12 +0800

    Feature: added constructor and attributes to the display and sensor class

--- src/car_park.py
+++ src/car_park.py
@@ -1,3 +1,12 @@
 
 class CarPark:
-    pass
+    def __init__(self, location, capacity, plates=None, sensors=None, displays=None):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates or []
+        self.sensors = sensors or []
+        self.displays = displays or []
+
+    def __str__(self):
+        # Return a string that contain the car park's location and capacity
+        return f"Location : { self.location }, capacity: { self.capacity }"
--- src/sensor.py
+++ src/sensor.py
@@ -1,3 +1,19 @@
 
 class Sensor:
+    def __init__(self, id, is_active, car_park):
+        self.id = id
+        self.is_active = is_active
+        self.car_park = car_park
+
+    def __str__(self):
+        return f"{self.id} is {'active' if self.is_active else 'not active'}"
+
+
+class EntrySensor(Sensor):
     pass
+
+
+class ExitSensor(Sensor):
+    pass
+
+
