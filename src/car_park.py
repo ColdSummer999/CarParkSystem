e59c7efd5584f36ead0869684b6a1f431b05f0e8
J153586 <J153586@tafe.wa.edu.au>
2024-11-09 16:41:07 +0800

    Added logging feature and test unit

--- src/car_park.py
+++ src/car_park.py
@@ -3,6 +3,7 @@
 from sensor import Sensor
 from display import Display
 from pathlib import Path
+from datetime import datetime
 
 
 class CarPark:
@@ -12,7 +13,9 @@ class CarPark:
         self.plates = plates or []
         self.sensors = sensors or []
         self.displays = displays or []
-        self.log_file = log_file
+        self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
+        # create the file if it doesn't exist:
+        self.log_file.touch(exist_ok=True)
 
     def __str__(self):
         # Return a string that contain the car park's location and capacity
@@ -30,11 +33,13 @@ class CarPark:
         # Add plate number and update the display
         self.plates.append(plate)
         self.update_displays()
+        self._log_car_activity(plate, "entered")
 
     def remove_car(self, plate):
         # Remove the plate number and update the display
         self.plates.remove(plate)
         self.update_displays()
+        self._log_car_activity(plate, "exited")
 
     @property
     def available_bays(self):
@@ -45,3 +50,7 @@ class CarPark:
         data = {"available bays": self.available_bays, "temperature": 25}
         for display in self.displays:
             display.update(data)
+
+    def _log_car_activity(self, plate, action):
+        with self.log_file.open("a") as f:
+            f.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
--- test/test_car_park.py
+++ test/test_car_park.py
@@ -9,8 +9,8 @@ DEBUG = False
 class TestCarPark(unittest.TestCase):
 
     def setUp(self):
-        self.car_park = CarPark("123 Example Street", 100)
         self.log_path = Path("new_log.txt")
+        self.car_park = CarPark("123 Example Street", 100, self.log_path)
 
     def test_car_park_initialized_with_all_attributes(self):
         self.assertIsInstance(self.car_park, CarPark)
@@ -80,14 +80,17 @@ class TestCarPark(unittest.TestCase):
         self.assertIn("\n", last_line)
 
     def test_car_logged_when_exiting(self):
-        new_carpark = CarPark("123 Example Street", 100, log_file="new_log.txt")
+        new_carpark = CarPark("123 Example Street", 100, self.log_path)
         self.car_park.add_car("NEW-001")
         self.car_park.remove_car("NEW-001")
         with self.car_park.log_file.open() as f:
             last_line = f.readlines()[-1]
-        self.assertIn(last_line, "NEW-001")
-        self.assertIn(last_line, "exited")
-        self.assertIn(last_line, "\n")
+        # check plate entered
+        self.assertIn("NEW-001", last_line)
+        # check description
+        self.assertIn("exited", last_line)
+        # check entry has a new line
+        self.assertIn("\n", last_line)
 
 
 if __name__ == '__main__':
