e8f1f10a8c898f4dcc5abfe717f8edcaa651bad3
J153586 <J153586@tafe.wa.edu.au>
2024-11-10 08:59:23 +0800

    Added config file and unti testing for config file

--- src/car_park.py
+++ src/car_park.py
@@ -4,10 +4,13 @@ from sensor import Sensor
 from display import Display
 from pathlib import Path
 from datetime import datetime
+import json
 
 
 class CarPark:
-    def __init__(self, location, capacity, log_file=Path("log.txt"), plates=None, sensors=None, displays=None):
+    def __init__(self, location, capacity, log_file=Path("log.txt"), config_file=Path("config.json"), plates=None
+                 , sensors=None, displays=None):
+
         self.location = location
         self.capacity = capacity
         self.plates = plates or []
@@ -16,6 +19,7 @@ class CarPark:
         self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
         # create the file if it doesn't exist:
         self.log_file.touch(exist_ok=True)
+        self.config_file = config_file
 
     def __str__(self):
         # Return a string that contain the car park's location and capacity
@@ -54,3 +58,16 @@ class CarPark:
     def _log_car_activity(self, plate, action):
         with self.log_file.open("a") as f:
             f.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
+
+    def write_config(self):
+        with open(self.config_file, "w") as f:
+            json.dump({"location": self.location,
+                       "capacity": self.capacity,
+                       "log_file": str(self.log_file)}, f)
+
+    @classmethod
+    def from_config(cls, config_file=Path("config.json")):
+        config_file = config_file if isinstance(config_file, Path) else Path(config_file)
+        with config_file.open() as f:
+            config = json.load(f)
+        return cls(config["location"], config["capacity"], log_file=config["log_file"])
